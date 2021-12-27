12039d0bd46b80abd1dbe27f4db893f0933cf90b
JustusMzB <ju@mzb.de>
2021-12-27 20:37:39 +0100

    Removed beautified lines for ppt

--- testapp/models.py
+++ testapp/models.py
@@ -7,7 +7,3 @@ class Parameter(models.Model):
     name = models.CharField(max_length=300)
     value = models.CharField(max_length=300)
     parameters = models.ForeignKey(HttpCall, on_delete=models.CASCADE)
-
-call = HttpCall.objects.get(url='fiktiver/pfad')
-call.parameter_set.create(name='test_parameter', value='test_wert')
-call.parameter_set.all()
