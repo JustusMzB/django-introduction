68fac105a0b5c5e54b3260d9055ed28bbda44a90
JustusMzB <ju@mzb.de>
2021-12-27 22:43:49 +0100

    registered models, defined __str__, made superuser

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import HttpCall, Parameter
 
-# Register your models here.
+admin.site.register([HttpCall, Parameter])
--- testapp/models.py
+++ testapp/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 class HttpCall(models.Model):
     url = models.URLField()
+    def __str__(self):
+        return self.url
+    
 
 class Parameter(models.Model):
     name = models.CharField(max_length=300)
     value = models.CharField(max_length=300)
-    parameters = models.ForeignKey(HttpCall, on_delete=models.CASCADE)
+    call = models.ForeignKey(HttpCall, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.name}={self.value}'
