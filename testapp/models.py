52053d7cbf9882243e149d7371968a5ba3618219
JustusMzB <ju@mzb.de>
2021-12-27 20:33:31 +0100

    Simplified the example models, did ops as in sheet

--- testapp/models.py
+++ testapp/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+class HttpCall(models.Model):
+    url = models.URLField()
+
 class Parameter(models.Model):
     name = models.CharField(max_length=300)
     value = models.CharField(max_length=300)
+    parameters = models.ForeignKey(HttpCall, on_delete=models.CASCADE)
 
-class HttpCall(models.Model):
-    url = models.URLField()
-    time = models.DateTimeField()
-    parameters = models.ForeignKey(Parameter, on_delete=models.CASCADE)
+call = HttpCall.objects.get(url='fiktiver/pfad')
+call.parameter_set.create(name='test_parameter', value='test_wert')
+call.parameter_set.all()
